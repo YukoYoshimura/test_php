dba221c60fced218e5da621149679d0bf2f39e3c
YukoYoshimura <101579747+YukoYoshimura@users.noreply.github.com>
2022-04-13 03:00:06 +0900

    q1

--- test.php
+++ test.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
+<?php
+
+  // 1. FizzBuzz
+  for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
+      echo 'FizzBuzz';
+      echo '<br>';
+    } elseif ($i % 3 === 0) {
+      echo 'Fizz';
+      echo '<br>';
+    } elseif ($i % 5 === 0) {
+      echo 'Buzz';
+      echo '<br>';
+    } else {
+      echo $i;
+      echo '<br>';
+    }
+  }
+
+  echo '<br><br>';
+
+  // 2. 素数算出
   
-</body>
-</html>
