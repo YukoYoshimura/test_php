adf249c66812fd56e913e41a3e126fc8b977d4e5
YukoYoshimura <101579747+YukoYoshimura@users.noreply.github.com>
2022-04-15 02:01:40 +0900

    q2,q3

--- test.php
+++ test.php
@@ -19,5 +19,71 @@
 
   echo '<br><br>';
 
+
   // 2. 素数算出
+  for ($i = 1; $i <= 10000; $i++) {
+    $point = 0;
+    for ($j = 1; $j <= $i; $j++) {
+      if($i % $j === 0) {
+        $point += 1;
+      }
+    }
+    if ($point === 2) {
+      echo $i;
+      echo '<br>';
+    }
+  }
+
+  echo '<br><br>';
   
+  
+  // 3. 5次元配列
+  $fruitsPrice = [
+    'orange' => [
+      'domestic' => [
+        'Wakayama' => [
+          'size-s' => ['a-store' => 100, 'b-store' => 120],
+          'size-m' => ['a-store' => 200, 'b-store' => 220],
+        ],
+        'Ehime' => [
+          'size-s' => ['a-store' => 150, 'b-store' => 170],
+          'size-m' => ['a-store' => 250, 'b-store' => 270],
+        ],
+      ],
+      'foreign' => [
+        'Brazil' => [
+          'size-s' => ['a-store' => 300, 'b-store' => 320],
+          'size-m' => ['a-store' => 350, 'b-store' => 370],
+        ],
+        'China' => [
+          'size-s' => ['a-store' => 350, 'b-store' => 370],
+          'size-m' => ['a-store' => 450, 'b-store' => 470],
+        ],
+      ],
+    ],
+    'banana' => [
+      'domestic' => [
+        'Okinawa' => [
+          'size-s' => ['a-store' => 100, 'b-store' => 120],
+          'size-m' => ['a-store' => 200, 'b-store' => 220],
+        ],
+        'Kagoshima' => [
+          'size-s' => ['a-store' => 150, 'b-store' => 170],
+          'size-m' => ['a-store' => 250, 'b-store' => 270],
+        ],
+      ],
+      'foreign' => [
+        'Philippines' => [
+          'size-s' => ['a-store' => 300, 'b-store' => 320],
+          'size-m' => ['a-store' => 350, 'b-store' => 370],
+        ],
+        'Ecuador' => [
+          'size-s' => ['a-store' => 350, 'b-store' => 370],
+          'size-m' => ['a-store' => 450, 'b-store' => 470],
+        ],
+      ],
+    ],
+  ];
+
+  // ex)国内・和歌山産みかん（Sサイズ）のa店の値段を出力
+  echo $fruitsPrice['orange']['domestic']['Wakayama']['size-s']['a-store'];
